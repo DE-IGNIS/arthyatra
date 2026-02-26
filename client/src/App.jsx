eea6af5fe4747512ede7bed044dbc4cae839b5fb
DE-IGNIS <chauhansudarsh@gmail.com>
2026-02-26 11:23:49 +0530

    made routing structure changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Content from "./pages/Content";
 import About from "./pages/About";
-import Simulator from "./pages/Simulator";
+import Simulator from "./pages/InvestmentSimulator";
+import SipCalculator from "./pages/SipCalculator";
+import RiskProfiler from "./pages/RiskProfiler";
+import MarketQuiz from "./pages/MarketQuiz";
 import Layout from "./layouts/Layout";
 
 function App() {
@@ -11,7 +14,10 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/simulator" element={<Simulator />} />
+          <Route path="/investment-simulator" element={<Simulator />} />
+          <Route path="/sip-calculator" element={<SipCalculator />} />
+          <Route path="/risk-profiler" element={<RiskProfiler />} />
+          <Route path="/market-quiz" element={<MarketQuiz />} />
           <Route path="/content" element={<Content />} />
           <Route path="/about" element={<About />} />
         </Route>
