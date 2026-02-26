1c3154dc6faf607da09b477bb74dc9c6e2196824
DE-IGNIS <chauhansudarsh@gmail.com>
2026-02-26 11:21:53 +0530

    TradingViewWidget added to main InvestmentSimulator tab

--- client/src/components/TradingViewWidget.jsx
+++ client/src/components/TradingViewWidget.jsx
@@ -1,22 +1,21 @@
-// TradingViewWidget.jsx
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useEffect, useRef, memo } from "react";
 
-function TradingViewWidget() {
+function TradingViewWidget({ stock_symbol }) {
   const container = useRef();
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
         {
-          "allow_symbol_change": true,
+          "allow_symbol_change": false,
           "calendar": false,
           "details": false,
           "hide_side_toolbar": true,
-          "hide_top_toolbar": false,
+          "hide_top_toolbar": true,
           "hide_legend": false,
           "hide_volume": false,
           "hotlist": false,
@@ -24,8 +23,8 @@ function TradingViewWidget() {
           "locale": "en",
           "save_image": true,
           "style": "1",
-          "symbol": "NASDAQ:GOOGL",
-          "theme": "dark",
+          "symbol": "${stock_symbol}",
+          "theme": "light",
           "timezone": "Etc/UTC",
           "backgroundColor": "#0F0F0F",
           "gridColor": "rgba(242, 242, 242, 0.06)",
@@ -35,15 +34,29 @@ function TradingViewWidget() {
           "studies": [],
           "autosize": true
         }`;
-      container.current.appendChild(script);
-    },
-    []
-  );
+    container.current.appendChild(script);
+  }, []);
 
   return (
-    <div className="tradingview-widget-container" ref={container} style={{ height: "100%", width: "100%" }}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
+    <div
+      className="tradingview-widget-container"
+      ref={container}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <div
+        className="tradingview-widget-container__widget"
+        style={{ height: "calc(100% - 32px)", width: "100%" }}
+      ></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href={`https://www.tradingview.com/chart/?symbol=${stock_symbol}`}
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">{stock_symbol} stock chart</span>
+        </a>
+        <span className="trademark"> by TradingView</span>
+      </div>
     </div>
   );
 }
