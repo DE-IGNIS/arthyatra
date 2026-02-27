91a537d23c17475305edb08a8e73432e3a7d21d0
DE-IGNIS <chauhansudarsh@gmail.com>
2026-02-27 23:10:54 +0530

    Add introductory content for financial literacy on the content page

--- client/src/pages/Content.jsx
+++ client/src/pages/Content.jsx
@@ -2,6 +2,7 @@ export default function Content() {
   return (
     <div>
       <h1>This is the content page</h1>
+      <h1>Basic content regarding financial literacy would be created here</h1>
     </div>
   );
 }
